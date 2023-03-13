d15351ba03758b4279806f0bd50ea3fbdff4aedd
Moritz Gubler <moritz.gubler@gmail.com>
2023-03-13 13:19:00 +0100

    started work on cli

--- sirius_ase/ase_simulation.py
+++ sirius_ase/ase_simulation.py
@@ -2,6 +2,12 @@ from ase import atoms
 import ase.io
 import os
 import sys
+import traceback
+import numpy as np
+import json
+import sirius_ase.siriusCalculator
+import argparse
+
 
 def aseSimulation(atoms: atoms.Atom, structufileName: str, outputFilename: str, startIndex:int =0, endIndex:int=-1
         , calc_energy = True, calc_forces = True, calc_stress = True):
@@ -53,3 +59,39 @@ def aseSimulation(atoms: atoms.Atom, structufileName: str, outputFilename: str, 
         # write file also to standard out in order to monitor progress
         # ase.io.write('-', atom_list[i], parallel=False)
         sys.stdout.flush()
+
+
+def entry():
+
+    siriusJsonFileName = sys.argv[1]
+    structufileName = sys.argv[2]
+    outputFilename = sys.argv[3]
+    if not os.path.exists(siriusJsonFileName):
+        print('json file does not exist')
+        quit()
+
+    atoms = ase.io.read(filename=structufileName, parallel=False, index = 0)
+
+    f = open(siriusJsonFileName)
+    jsonparams = json.load(f)
+    f.close()
+    try:
+        pp_files = jsonparams["unit_cell"]["atom_files"]
+        pw_cutoff = jsonparams['parameters']["pw_cutoff"]
+        gk_cutoff = jsonparams['parameters']["gk_cutoff"]
+        functionals = jsonparams['parameters']['xc_functionals']
+        kpoints = jsonparams['parameters']['ngridk']
+        kshift = jsonparams['parameters']["shiftk"]
+        if "atom_types" in jsonparams["unit_cell"]:
+            jsonparams["unit_cell"].pop("atom_types")
+        jsonparams["unit_cell"].pop("atom_files")
+    except KeyError:
+        print("required parameter was missing")
+        traceback.print_exc()
+        quit()
+
+    atoms.calc = sirius_ase.siriusCalculator.SIRIUS(atoms, pp_files, functionals, kpoints, kshift, pw_cutoff, gk_cutoff, jsonparams)
+
+    sirius_ase.ase_simulation.aseSimulation(atoms, structufileName, outputFilename)
+
+    atoms.calc.close()
