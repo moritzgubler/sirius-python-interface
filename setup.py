748f5fadf676819fb16cbf638da5dc4fab1b286f
Moritz Gubler <moritz.gubler@gmail.com>
2023-03-13 16:18:36 +0100

    Added cli file to setup.py

--- setup.py
+++ setup.py
@@ -13,16 +13,9 @@ setup(
                       'numpy',
                       'ase',         
                       ],
-
-    # classifiers=[
-    #     'Development Status :: 1 - Planning',
-    #     'Intended Audience :: Science/Research',
-    #     'License :: OSI Approved :: BSD License',  
-    #     'Operating System :: POSIX :: Linux',        
-    #     'Programming Language :: Python :: 2',
-    #     'Programming Language :: Python :: 2.7',
-    #     'Programming Language :: Python :: 3',
-    #     'Programming Language :: Python :: 3.4',
-    #     'Programming Language :: Python :: 3.5',
-    # ],
+    entry_points={
+      'console_scripts': [
+        'SiriusSinglePoint=sirius_ase.ase_simulation:entry'
+      ]
+    }
 )
